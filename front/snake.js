6241d5950c10012e6225c4330a5804a8e82a86d2
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2021-06-12 22:26:28 -0400

    Merge Edward's code
    Got semi-functional game

--- front/snake.js
+++ front/snake.js
@@ -1,165 +1,177 @@
-const maxWidth = 640; // document.body.clientWidth;
-const maxHeight = 400; // document.body.clientHeight;
-const squareSize = 20;
-
-const createCanvas = () => {
-  const canvas = document.getElementById('snake-canvas');
-  canvas.width = maxWidth;
-  canvas.height = maxHeight;
-
-  return canvas.getContext('2d');
+const gameGlobals = {
+  fps: 1000 / 15, // by 15 gives about >60 fps
+  squareSize: 20,
+  playerTail: 5,
 };
 
+const state = {};
 
-const ctx = createCanvas();
+const Position = ({x = 0, y = 0}) => {
+  return {
+    x,
+    y,
+  };
+};
+
+const getRandomPosition = () => {
+  const x = Math.floor(Math.random() * state.ctx.canvas.width);
+  const y = Math.floor(Math.random() * state.ctx.canvas.height);
+  const pos = Position({x, y});
+  // land in the grid
+  pos.x = pos.x - (pos.x % gameGlobals.squareSize);
+  pos.y = pos.y - (pos.y % gameGlobals.squareSize);
+
+  return pos;
+};
 
 const Board = () => {
+  const ctx = state.ctx;
   const draw = () => {
     ctx.beginPath();
     ctx.fillStyle = 'black';
-    ctx.rect(0, 0, maxWidth, maxHeight);
+    ctx.rect(0, 0, ctx.canvas.width, ctx.canvas.height);
     ctx.fill();
-    ctx.restore();
   };
 
   return { draw };
 };
 
-const Meal = ({ size, x, y }) => {
-  const getPosition = () => ({ x, y });
+const Food = ({
+  size = gameGlobals.squareSize,
+  color = 'red',
+  pos,
+}) => {
+  const ctx = state.ctx;
+  const getPosition = () => pos;
 
   const draw = () => {
     ctx.beginPath();
-    ctx.fillStyle = 'red';
-    ctx.rect(x, y, size, size);
+    ctx.fillStyle = color;
+    ctx.rect(pos.x, pos.y, size, size);
     ctx.fill();
-    ctx.restore();
   };
 
-  return { getPosition, draw };
+  return {
+    getPosition,
+    draw,
+  };
 };
 
-const Snake = ({ size, color = 'green', x, y }) => {
-  let body = [ { x, y }, { x, y }, { x, y } ]; // array con objetos { x, y }
-  let xVel = 0;
-  let yVel = 0;
+const Snake = ({ size, color = 'lime', pos }) => {
+  const ctx = state.ctx;
+  let body = [ Position(pos), Position(pos), Position(pos) ];
+  const velocity = { x: 0, y: 0 };
 
-
-  const updatePart = ({ x, y }) => ({
-    x: (x + size * Math.sign(xVel) + maxWidth) % maxWidth,
-    y: (y + size * Math.sign(yVel) + maxHeight) % maxHeight,
+  const updatePart = pos => ({
+    x: (pos.x + size * Math.sign(velocity.x) +
+      ctx.canvas.width) % ctx.canvas.width,
+    y: (pos.y + size * Math.sign(velocity.y) +
+      ctx.canvas.height) % ctx.canvas.height,
   });
 
-  const eat = meal => body.unshift(updatePart(meal.getPosition()));
+  const eat = food => body.unshift(updatePart(food.getPosition()));
 
   const getHead = () => body[0];
 
   const update = () => {
-    body.pop(); // eliminar cola
-    body.unshift(updatePart(getHead())); // nueva cabeza
+    body.pop(); // remove tail
+    body.unshift(updatePart(getHead())); // new head
   };
 
   const draw = () => {
     ctx.beginPath();
     ctx.fillStyle = color;
-    body.forEach(({ x, y }) => {
-      ctx.rect(x, y, size, size);
+    body.forEach(pos => {
+      ctx.rect(pos.x, pos.y, size, size);
       ctx.fill();
     });
-    ctx.restore();
-  };
-
-  const isStarving = () => {
-    const head = getHead();
-
-    return !body.every(({ x, y }) => x === head.x && y === head.y) &&
-      body.slice(1).find(({ x, y }) => x === head.x && y === head.y);
   };
 
   const reset = () => {
-    body = [ { x, y }, { x, y }, { x, y } ];
-    xVel = 0;
-    yVel = 0;
+    body = [ Position(pos), Position(pos), Position(pos) ];
+    velocity.x = 0;
+    velocity.y = 0;
   };
 
-  const turnLeft = () => { xVel = -1; yVel = 0; };
+  const turnLeft = () => { velocity.x = -1; velocity.y = 0; };
 
-  const turnRight = () => { xVel = 1; yVel = 0; };
+  const turnRight = () => { velocity.x = 1; velocity.y = 0; };
 
-  const turnUp = () => { xVel = 0; yVel = -1; };
+  const turnUp = () => { velocity.x = 0; velocity.y = -1; };
 
-  const turnDown = () => { xVel = 0; yVel = 1; };
+  const turnDown = () => { velocity.x = 0; velocity.y = 1; };
 
-  return { getHead, draw, eat, update, turnDown, turnLeft, turnRight, turnUp, isStarving, reset };
+  return {
+    getHead,
+    draw,
+    eat,
+    update,
+    turnDown,
+    turnLeft,
+    turnRight,
+    turnUp,
+    reset,
+  };
 };
-
-const getRandomPosition = () => {
-  let x = Math.floor(Math.random() * maxWidth);
-  x = x - (x % squareSize);
-  let y = Math.floor(Math.random() * maxHeight);
-  y = y - (y % squareSize);
-
-  return { x, y };
-};
-
-const mySnake = Snake({
-  size: squareSize,
-  color: 'green',
-  ...getRandomPosition(),
-});
-
-let prevKey;
-document.onkeydown = e => {
-  e = e || window.event;
-
-  const key = e.key;
-  if (key === 'ArrowLeft' && prevKey !== 'ArrowRight') {
-    mySnake.turnLeft();
-    prevKey = key;
-  } else if (key === 'ArrowUp' && prevKey !== 'ArrowDown') {
-    mySnake.turnUp();
-    prevKey = key;
-  } else if (key === 'ArrowRight' && prevKey !== 'ArrowLeft') {
-    mySnake.turnRight();
-    prevKey = key;
-  } else if (key === 'ArrowDown' && prevKey !== 'ArrowUp') {
-    mySnake.turnDown();
-    prevKey = key;
-  }
-};
-
-const board = Board();
-let meal;
 
 const game = () => {
-  board.draw();
+  const { canvas, board, food, snake } = state;
+  canvas.width = Math.max(document.body.clientWidth ||
+    0, window.innerWidth || 0);
+  canvas.height = Math.max(document.body.clientHeight ||
+    0, window.innerHeight || 0);
 
+  if(food.length) {
+    const sPos = snake.getHead();
 
-  // comer o generar comida
-  if (meal) {
-    const snakePosition = mySnake.getHead();
-    const mealPosition = meal.getPosition();
-
-    if (snakePosition.x === mealPosition.x && snakePosition.y === mealPosition.y) {
-      mySnake.eat(meal);
-      meal = undefined;
+    let foodToDelete;
+    food.forEach((f, i) => {
+      const fPos = f.getPosition();
+      if (sPos.x === fPos.x && sPos.y === fPos.y) {
+        snake.eat(f);
+        foodToDelete = i;
+      }
+    });
+    // delete food
+    if (foodToDelete >= 0) {
+      food.splice(foodToDelete, 1);
+      // create a new food
+      food.push(Food({pos: getRandomPosition()}));
     }
-  } else {
-    meal = Meal({ size: squareSize, ...getRandomPosition() });
   }
 
-  // suicidio por hambre :c
-  if (mySnake.isStarving()) {
-    mySnake.reset();
-  }
-
-
-  meal?.draw();
-  mySnake.draw();
-  mySnake.update();
+  board.draw();
+  food.forEach(f => f.draw());
+  snake.update();
+  snake.draw();
 };
 
-setInterval(game, 1000 / 15);
+const keyPush = e => {
+  const snake = state.snake;
+  const key = e.key;
+  if (key === 'ArrowLeft') {
+    snake.turnLeft();
+  } else if (key === 'ArrowUp') {
+    snake.turnUp();
+  } else if (key === 'ArrowRight') {
+    snake.turnRight();
+  } else if (key === 'ArrowDown') {
+    snake.turnDown();
+  }
+};
 
-// igual se puede devolver si se apretan 2 teclas rápido! (te podés morir :o)
-// hay que evitar que los puntitos come come salgan bajo las serpientes
+window.onload = () => {
+  state.canvas = document.getElementById('snake');
+  state.ctx = state.canvas.getContext('2d');
+  state.board = Board();
+  state.snake = Snake({
+    size: gameGlobals.squareSize,
+    pos: getRandomPosition(),
+  });
+  state.food = [Food({pos: getRandomPosition()})];
+
+  document.addEventListener('keydown', keyPush);
+  setInterval(() => game(), gameGlobals.fps);
+};
+
+
